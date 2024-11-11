cb1cb1e8a4a32fc9ed97d5b2aea58f8aa0d5c662
sermsak <tu.sermsak@gmail.com>
2024-11-11 22:23:09 +0700

    feat:amswer

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,13 @@
 // Exercise #3: Find a Minimum Score
 let scores = [100, 20, 3, 1000];
 let minScore;
+let scoresStart;
 // Start coding here
+for(let i=0;i<scores.length;i=i+1){
+    if(scoresStart>scores[i]){
+        minScore=scores[i];
+    }
+    scoresStart=scores[i];
 
+}
 console.log(minScore);
--- ex-6.js
+++ ex-6.js
@@ -1,7 +1,9 @@
-// Exercise #6: Reverse the String
 let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
+for (let i = 0; i < companyName.length; i++) {
+  reversedCompanyName = companyName[i] + reversedCompanyName;
+}
 
 console.log(reversedCompanyName);
